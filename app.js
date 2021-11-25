82f21dce66ece304dafa41013ec79ec0b2b856df
abrormukimov <abrormm34@gmail.com>
2021-11-25 09:05:31 +0500

    Add localstorage and a function to delete books

--- app.js
+++ app.js
@@ -1,53 +1,93 @@
-const innerContainer = document.querySelector('.inner-ctn');
-const form = document.querySelector('.form');
-
-let books = [];
+const booksCtn = document.querySelector('.books-ctn');
+const form = document.getElementById('form');
+let myBooks = [];
 
 function Book(title, author) {
   this.title = title;
   this.author = author;
 }
 
-let addBook = (book) => {
-  const bookContainer = document.createElement('div');
-  const bookTitle = document.createElement('p');
-  const bookAuthor = document.createElement('p');
-  const button = document.createElement('button');
+const render = (book) => {
+  const bookSection = document.createElement('div');
+  const title = document.createElement('p');
+  const author = document.createElement('p');
+  const remove = document.createElement('button');
   const hr = document.createElement('hr');
 
-  bookContainer.classList.add('book-ctn');
-  bookTitle.classList.add('book-title');
-  bookAuthor.classList.add('book-author');
-  button.classList.add('remove-btn');
+  bookSection.classList.add('book-section');
+  title.classList.add('book-title');
+  author.classList.add('book-author');
+  remove.classList.add('remove');
+  remove.setAttribute('data-title', `${book.title}`);
 
-  bookTitle.innerHTML = `${book.title}`;
-  bookAuthor.innerHTML = `${book.author}`;
-  button.innerHTML = 'Remove';
+  title.textContent = `${book.title}`;
+  author.textContent = `${book.author}`;
+  remove.textContent = 'Remove';
 
-  innerContainer.appendChild(bookContainer);
-  innerContainer.appendChild(bookTitle);
-  innerContainer.appendChild(bookAuthor);
-  innerContainer.appendChild(button);
-  innerContainer.appendChild(hr);
+  booksCtn.appendChild(bookSection);
+  bookSection.appendChild(title);
+  bookSection.appendChild(author);
+  bookSection.appendChild(remove);
+  bookSection.appendChild(hr);
 };
 
-const removeBook = (title) => {
-  books = books.filter((book) => book.title === title);
+const clearInput = () => {
+  const title = document.querySelector('.title');
+  const author = document.querySelector('.author');
+  title.value = '';
+  author.value = '';
+};
+
+const getFromLocalStorage = () => {
+  const books = JSON.parse(localStorage.getItem('myBooks')) || [];
   return books;
 };
 
+const saveToLocalStorage = (book) => {
+  const books = getFromLocalStorage();
+  books.push(book);
+  myBooks = books;
+  localStorage.setItem('myBooks', JSON.stringify(myBooks));
+};
+
+const displayBooks = () => {
+  myBooks = getFromLocalStorage();
+  myBooks.forEach((book) => {
+    render(book);
+  });
+};
+
+displayBooks();
+
+const removeBook = (title) => {
+  const store = getFromLocalStorage();
+  for (let i = 0; i < store.length; i += 1) {
+    if (store[i].title === title) {
+      store.splice(i, 1);
+    }
+  }
+  myBooks = store;
+  localStorage.setItem('myBooks', JSON.stringify(myBooks));
+};
+
+document.addEventListener('HTMLContentLoaded', displayBooks);
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-
-  const inputTitle = document.querySelector('.title').value;
-  const inputAuthor = document.querySelector('.author').value;
-  const book = new Book(inputTitle, inputAuthor);
-  books.push(book);
-  addBook(book);
+  const title = document.querySelector('.title').value;
+  const author = document.querySelector('.author').value;
+  const book = new Book(title, author);
+  render(book);
+  saveToLocalStorage(book);
+  clearInput();
 });
 
-document.addEventListener('HTMLContentLoaded', () => {
-  books.forEach((book) => {
-    addBook(book);
+if (myBooks) {
+  booksCtn.addEventListener('click', (e) => {
+    if (e.target.classList.contains('remove')) {
+      e.target.parentElement.remove();
+      const title = e.target.getAttribute('data-title');
+      removeBook(title);
+    }
   });
-});
+}
